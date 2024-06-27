a1ade6fc9cd711c91115346f6ad0d7c9fe4a1d5e
personontheinternet1234 <135486733+personontheinternet1234@users.noreply.github.com>
2024-06-27 08:21:50 -1000

    added payload

--- main.py
+++ main.py
@@ -3,19 +3,17 @@ import requests
 
 proxies = {'https': 'http://127.0.0.1:8888'}
 
+payload = {
+"model": "tinyllama",
+"prompt": "test"
+}
+
 # The API endpoint
 url = "http://localhost:11434/api/generate"
 
 # A GET request to the API
-response = requests.get(url, verify=False, proxies=proxies)
+response = requests.get(url, params=payload)
 
 # Print the response
 response_json = response.json()
 print(response_json)
-
-"""
-{
-"model": "tinyllama",
-"prompt": "test"
-}
-"""
