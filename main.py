eea8b7c23e4753675b84af8d2701885ac9b074ac
personontheinternet1234 <135486733+personontheinternet1234@users.noreply.github.com>
2024-06-27 08:10:07 -1000

    changed "proxies" to fix ssl version error - possible

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import tensorflow as tf
 import requests
 
+proxies = {'https': 'http://127.0.0.1:8888'}
+
 # The API endpoint
 url = "http://localhost:11434/api/generate"
 
 # A GET request to the API
-response = requests.get(url)
+response = requests.get(url, verify=False, proxies=proxies)
 
 # Print the response
 response_json = response.json()
