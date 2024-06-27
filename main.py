4141d1e3ba61be363388626c20811a5cd115d92e
personontheinternet1234 <135486733+personontheinternet1234@users.noreply.github.com>
2024-06-27 08:40:02 -1000

    trying to see what this does

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ data = {
 # The API endpoint
 url = "http://localhost:11434/api/generate"
 
-# A POST request to tthe API
+# A POST request to the API
 response = requests.post(url, json=data)
 
 # Print the response
 print(response)
-response_content = response.content
-print(response_content)
 response_text = response.text
 print(response_text)
 
+for line in response_text:
+    print("LOL" + line)
